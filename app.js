60b488ef3395ad0a4a798354a3e9fbdca35c4a4f
Stanislav Ivanov <103877318+stanislaviw@users.noreply.github.com>
2022-08-01 12:37:45 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+
+
+let button = document.querySelector('.btn');
+let image = document.querySelector('.img');
+let url = 'http://aws.random.cat//meow';
+
+async function fetchHandler() {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        image.src = data.file;
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+button.addEventListener('click', () => {
+    let isLoaded = image.complete;
+    
+    if(isLoaded) {
+        fetchHandler();
+    }
+});
+
